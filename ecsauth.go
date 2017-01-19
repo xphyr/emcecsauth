981ab274f1f86bac6b7b3d96987570f1596c2588
markd <mdeneve@paychex.com>
2017-01-19 09:25:40 -0500

    Initial conntection against ECS endpoint works.

--- ecsauth.go
+++ ecsauth.go
@@ -15,41 +15,67 @@ import (
 func main() {
 	// Setup the basic command line arguments
 	serverPtr := flag.String("server", "server.example.com", "ECS Cluster to Connect to")
+	userNamePtr := flag.String("username", "user@example.com", "Username to authenticate as")
 	flag.Parse()
 
-	username, password := credentials()
-	fmt.Printf("Username: %s, Password: %s\n", username, password)
+	username, password := credentials(*userNamePtr)
+	fmt.Printf("Username: %s\n", username)
 
 	// GET request
 	// Basic Auth for all request
 	resty.SetBasicAuth(username, password)
+	resty.RemoveProxy()
 
-	reqBaseURL := "https://" + *serverPtr + ":4443/"
+	reqBaseURL := "https://" + *serverPtr + ":4443"
 	reqLoginURL := reqBaseURL + "/login"
+	reqKeyURL := reqBaseURL + "/object/secret-keys"
+	fmt.Println("Login URL: " + reqLoginURL)
 	resp, err := resty.R().Get(reqLoginURL)
 
+	authToken := resp.Header()["X-Sds-Auth-Token"][0]
+
+	// explore response object
+	// fmt.Printf("\nError: %v", err)
+	// fmt.Printf("\nResponse Status Code: %v", resp.StatusCode())
+	// fmt.Printf("\nResponse Status: %v", resp.Status())
+	// fmt.Printf("\nResponse Time: %v", resp.Time())
+	// fmt.Printf("\nResponse Recevied At: %v", resp.ReceivedAt())
+	fmt.Println("\nRespone AuthToken: ", authToken)
+	// fmt.Printf("\nResponse Body: %v", resp.String()) // or resp.String() or string(resp.Body())
+
+	//lets try getting the current auth-Tokens
+	resty.SetHeader("Accept", "application/json")
+	resty.SetHeaders(map[string]string{
+		"Content-Type":     "application/json",
+		"X-SDS-AUTH-TOKEN": authToken,
+	})
+
+	resp, err = resty.R().Get(reqKeyURL)
 	// explore response object
 	fmt.Printf("\nError: %v", err)
 	fmt.Printf("\nResponse Status Code: %v", resp.StatusCode())
 	fmt.Printf("\nResponse Status: %v", resp.Status())
 	fmt.Printf("\nResponse Time: %v", resp.Time())
 	fmt.Printf("\nResponse Recevied At: %v", resp.ReceivedAt())
-	fmt.Printf("\nResponse Body: %v", resp) // or resp.String() or string(resp.Body())
+	fmt.Printf("\nResponse Body: %v", resp.String()) // or resp.String() or string(resp.Body())
 
 }
 
-func credentials() (string, string) {
+func credentials(username string) (string, string) {
 	reader := bufio.NewReader(os.Stdin)
+	password := ""
 
-	fmt.Print("Enter Username: ")
-	username, _ := reader.ReadString('\n')
-
+	if username == "" {
+		fmt.Print("Enter Username: ")
+		username, _ = reader.ReadString('\n')
+	}
 	fmt.Print("Enter Password: ")
 	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
 	if err == nil {
-		fmt.Println("\nPassword typed: " + string(bytePassword))
+		password = string(bytePassword)
+	} else {
+		fmt.Println("Error capturing password")
 	}
-	password := string(bytePassword)
 
 	return strings.TrimSpace(username), strings.TrimSpace(password)
 }
