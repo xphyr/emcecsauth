876511cce5451ed451e7170d4746cc7b71eca327
Mark DeNeve <markd@xphyr.net>
2017-01-18 21:16:15 -0500

    Initial checkin of code

---
+++ README.md
@@ -0,0 +1,5 @@
+Simple client to connect to the EMC ECS and retrieve S3 Key
+
+checkout the code
+run go get
+run go build
---
+++ ecsauth.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+func main() {
+	username, password := credentials()
+	fmt.Printf("Username: %s, Password: %s\n", username, password)
+
+	// GET request
+	// Basic Auth for all request
+	resty.SetBasicAuth(username, password)
+
+	resp, err := resty.R().Get("https://objlab.paychex.com/login")
+
+	// explore response object
+	fmt.Printf("\nError: %v", err)
+	fmt.Printf("\nResponse Status Code: %v", resp.StatusCode())
+	fmt.Printf("\nResponse Status: %v", resp.Status())
+	fmt.Printf("\nResponse Time: %v", resp.Time())
+	fmt.Printf("\nResponse Recevied At: %v", resp.ReceivedAt())
+	fmt.Printf("\nResponse Body: %v", resp) // or resp.String() or string(resp.Body())
+
+}
+
+func credentials() (string, string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter Username: ")
+	username, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter Password: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	if err == nil {
+		fmt.Println("\nPassword typed: " + string(bytePassword))
+	}
+	password := string(bytePassword)
+
+	return strings.TrimSpace(username), strings.TrimSpace(password)
+}
